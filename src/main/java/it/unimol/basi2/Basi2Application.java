a5d73dfee0007fb454d8d3e5e9154fa1a5e1570a
Yuri <mario@feeeg.com>
2025-03-11 11:50:04 +0100

    Inital commit,used Spring initlializer for dependencies

---
+++ src/main/java/it/unimol/basi2/Basi2Application.java
@@ -0,0 +1,13 @@
+package it.unimol.basi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Basi2Application.class, args);
+	}
+
+}
