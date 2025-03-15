d53a1ccd34bc8348d471343382c34204243f4b3b
Yuri <mario@feeeg.com>
2025-03-15 23:47:11 +0100

    Implementazione l'entità Prenotazione

--- src/main/java/it/unimol/basi2/entity/Prenotazione.java
+++ src/main/java/it/unimol/basi2/entity/Prenotazione.java
@@ -1,4 +1,20 @@
 package it.unimol.basi2.entity;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@AllArgsConstructor
+
 public class Prenotazione {
+    @Id
+    private String id;
+    private String eventoId; // Riferimento all'evento prenotato
+    private String partecipanteId; // Riferimento al partecipante
+    private LocalDateTime dataPrenotazione;
+    private int numeroBiglietti;
 }
