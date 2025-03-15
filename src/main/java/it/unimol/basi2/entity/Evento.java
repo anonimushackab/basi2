aac5a2a903e8098e225d96ef54ad1f9117b67bad
Yuri <mario@feeeg.com>
2025-03-15 22:36:45 +0100

    Implementata l'entità Evento

--- src/main/java/it/unimol/basi2/entity/Evento.java
+++ src/main/java/it/unimol/basi2/entity/Evento.java
@@ -1,5 +1,24 @@
 package it.unimol.basi2.entity;
 
-public class Evento {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
 
+
+@Data
+@Document
+@AllArgsConstructor
+
+public class Evento {
+    @Id
+    private String id;
+    private String nome;
+    private String descrizione;
+    private LocalDateTime data;
+    private String luogo;
+    private int capacitaMassima;
+    private int postiDisponibili;
+    private double prezzo;
 }
