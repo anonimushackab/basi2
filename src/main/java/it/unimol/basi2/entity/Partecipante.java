c2b4cf3b71eb274c1b52cd1fe19cea423f4a37bc
Yuri <mario@feeeg.com>
2025-03-15 23:12:15 +0100

    Implementata l'entita Partecipante

--- src/main/java/it/unimol/basi2/entity/Partecipante.java
+++ src/main/java/it/unimol/basi2/entity/Partecipante.java
@@ -1,4 +1,22 @@
 package it.unimol.basi2.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@AllArgsConstructor
+
 public class Partecipante {
+    @Id
+    private String id;
+    private String nome;
+    private String cognome;
+    private String telefono;
+    
+    @Indexed(unique = true)
+    private String email;
 }
