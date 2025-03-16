e3addc7fa252a94376a3b6f6e812be4389530261
Yuri <mario@feeeg.com>
2025-03-16 15:22:57 +0100

    Implementato PrenotazioneRepository

--- src/main/java/it/unimol/basi2/repository/PrenotazioneRepository.java
+++ src/main/java/it/unimol/basi2/repository/PrenotazioneRepository.java
@@ -1,4 +1,19 @@
 package it.unimol.basi2.repository;
 
-public interface PrenotazioneRepository {
+import it.unimol.basi2.entity.Prenotazione;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PrenotazioneRepository extends MongoRepository<Prenotazione, String> {
+    Optional<Prenotazione> findById(String id);
+
+    Optional<List<Prenotazione>> findByNomeCliente(String nomeCliente);
+
+    Optional<List<Prenotazione>> findByDataPrenotazione(LocalDateTime dataPrenotazione);
+
 }
