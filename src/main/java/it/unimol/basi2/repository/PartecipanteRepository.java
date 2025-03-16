6719b3f4f450d52f5684234228341cf51d5953c3
Yuri <mario@feeeg.com>
2025-03-16 14:46:08 +0100

    Implementato PartecipanteRepository

--- src/main/java/it/unimol/basi2/repository/PartecipanteRepository.java
+++ src/main/java/it/unimol/basi2/repository/PartecipanteRepository.java
@@ -1,4 +1,16 @@
 package it.unimol.basi2.repository;
+import it.unimol.basi2.entity.*;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PartecipanteRepository {
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PartecipanteRepository extends MongoRepository<Partecipante, String> {
+    Optional<Partecipante> findByEmail(String email);
+
+    Optional<Partecipante> findById(String id);
+
+    Optional<List<Partecipante>> findByNome(String nome);
 }
