7a1f222e49eb6af5d0c2036ee1aefa5d9e731f89
Yuri <mario@feeeg.com>
2025-03-16 01:38:41 +0100

    Implemetato EventoRepository

--- src/main/java/it/unimol/basi2/repository/EventoRepository.java
+++ src/main/java/it/unimol/basi2/repository/EventoRepository.java
@@ -1,4 +1,20 @@
 package it.unimol.basi2.repository;
 
-public interface EventoRepository {
+import it.unimol.basi2.entity.*;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.Query;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EventoRepository extends MongoRepository<Evento, String> {
+    Optional<Evento> findByNome(String nome);
+
+    
+    Optional<List<Evento>> findByLuogo(String luogo);
+
+    @Query("{ 'nome': ?0 }")
+    Optional<Evento> trovaPerNome(String nome);
+
 }
