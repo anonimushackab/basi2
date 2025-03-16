56de978a481372fb253159d9dd9e9da78cb2e925
Yuri <mario@feeeg.com>
2025-03-16 16:50:00 +0100

    Aggiunta di metodi fondamentali in EventoRepository

--- src/main/java/it/unimol/basi2/repository/EventoRepository.java
+++ src/main/java/it/unimol/basi2/repository/EventoRepository.java
@@ -4,15 +4,29 @@ import it.unimol.basi2.entity.*;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.Query;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EventoRepository extends MongoRepository<Evento, String> {
-    Optional<Evento> findByNome(String nome);
 
-    
-    Optional<List<Evento>> findByLuogo(String luogo);
+    Optional<Evento> findByNomeIgnoreCase(String nome);
 
+    List<Evento> findByLuogo(String luogo);
 
+    List<Evento> findByDataGreaterThanEqual(LocalDateTime data);
+
+    List<Evento> findByDataBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    List<Evento> findByPostiDisponibiliGreaterThan(int postiDisponibili);
+
+    @Query("{ 'prezzo': { $lt: ?0 } }")
+    List<Evento> findEventiConPrezzoInferioreA(double prezzo);
+
+    @Query("{ 'nome': { $regex: ?0, $options: 'i' } }")
+    List<Evento> findEventiByNomeContaining(String nome);
+
+    Optional<Evento> findById(String id);
 }
