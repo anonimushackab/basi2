573349ef82864ecac868ccdd57771833b0a76fac
Yuri <github-actions@github.com>
2025-03-22 21:25:51 +0100

    Aggiunta della disabilitazione della configurazione automatica del DataSource per i database relazionali

--- src/main/java/it/unimol/basi2/Basi2Application.java
+++ src/main/java/it/unimol/basi2/Basi2Application.java
@@ -2,8 +2,10 @@ package it.unimol.basi2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Basi2Application {
 
 	public static void main(String[] args) {
